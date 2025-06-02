2cee12745d822d6be000a3cc40d270c7a7df2146
Guga's PC <Guga's PC@DESKTOP-2DB9EPL>
2025-06-02 11:46:51 -0700

    Update

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestDockerNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("getNew")]
+        public async Task<IActionResult> Get()
+        {
+            // Simulate a delay to mimic a long-running operation
+            await Task.Delay(1000);
+            return Ok(new { Message = "Hello from ValuesController!" });
+        }
+    }
+}
